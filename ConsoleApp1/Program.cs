c0d049b9de21ec2452db2018bb012848164e4153
palak2110 <palakarora2110@gmail.com>
2019-06-11 13:31:59 +0530

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class sample
+    {
+        public void Show()
+        {
+            Console.WriteLine("hi this is my first program");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sample sap = new sample();
+            sap.Show();
+        }
+    }
+}
